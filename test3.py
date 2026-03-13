a8dc5b1e961070613f80796cbdc40568bd9faa2d
Forrest-wzy <3630046182@qq.com>
2026-03-13 17:05:31 +0800

    完成任务三 CNN

--- test3.py
+++ test3.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import torchvision
 from fontTools.misc.iterTools import batched
 from sklearn.model_selection import train_test_split
+from torch.cuda import device
 from torch.utils.data import DataLoader
 #torchvision
 from torchvision import datasets
@@ -19,7 +20,7 @@ transform = transforms.Compose([
 
 # 2. 下载/加载训练集
 train_set = datasets.CIFAR10(
-    root='D:/pytorch/data',       # 存到当前目录的 data 文件夹
+    root='D:\myproject\data',       # 存到当前目录的 data 文件夹
     train=True,          # 这是训练集
     download=False,
     transform=transform  # 应用预处理
@@ -27,7 +28,7 @@ train_set = datasets.CIFAR10(
 
 # 3. 下载/加载测试集
 test_set = datasets.CIFAR10(
-    root='D:/pytorch/data',
+    root='D:\myproject\data',
     train=False,         # 这是测试集
     download=False,
     transform=transform
@@ -100,3 +101,85 @@ for epoch in range(10):
 
         plt.show(block=True)
 visualize_predictions(model,test_loader,num_imagines=10)
+
+
+#CNN模型
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN,self).__init__()
+        self.model=nn.Sequential(
+            #3-16
+            nn.Conv2d(3,16,kernel_size=3,padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            #16-32
+            nn.Conv2d(16,32,kernel_size=3,padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            #32-64
+            nn.Conv2d(32,64,kernel_size=3,padding=1),
+            nn.MaxPool2d(2),
+        )
+        self.fl=nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(64*4*4,256),
+            nn.ReLU(),
+            nn.Linear(256,10)
+        )
+    def forward(self,x):
+        x=self.model(x)
+        print("卷积后的尺寸",x.shape)
+        x=self.fl(x)
+        return x
+
+device=torch.device('cuda'if torch.cuda.is_available()else'cpu')
+print(f"Using device:{device}")
+model=CNN()
+model=model.to(device)
+#损失函数
+criterion=nn.CrossEntropyLoss()
+#优化器
+learning_rate=0.001
+optimizer=optim.Adam(model.parameters(),lr=learning_rate)
+
+#训练模型
+print("begin CNN")
+train_losses=[]
+for epoch in range(10):
+    running_loss=0.0
+    for i,(images,labels) in enumerate(train_loader):
+        images,labels=images.to(device),labels.to(device)
+
+
+        outputs=model(images)
+        loss = criterion(outputs, labels)
+
+        # 反向传播
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+    avg_loss = running_loss / len(train_loader)
+    train_losses.append(avg_loss)
+    print(f'Epoch[{epoch+1}/10],Loss:{avg_loss:.4f}')
+
+#测试
+correct=0
+total=0
+with torch.no_grad():
+    for images,labels in test_loader:
+        images,labels=images.to(device),labels.to(device)
+        outputs=model(images)
+        _,predicted=torch.max(outputs,1)
+        total +=labels.size(0)
+        correct +=(predicted==labels).sum().item()
+print(f'\n准确率：{100*correct/total:.2f}%')
+
+#损失曲线
+plt.plot(train_losses)
+plt.title('CNN training loss')
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.savefig('CNN_loss.png')
+plt.show(block=True)
